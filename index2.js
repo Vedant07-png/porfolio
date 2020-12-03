91ca2302e7620833886103e6a6550bec4b3d3efb
Vedant07-png <69086233+Vedant07-png@users.noreply.github.com>
2020-12-03 14:15:09 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,16 @@
+<html lang="en">
+   <head>
+   </head>
+         <body>
+               <div class= "jumbotron text-centre">
+                     <div class="display-4">
+                           javascript es6
+
+                     </div>
+
+               </div>
+
+               
+        </body>   
+           
+</html>
